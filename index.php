f2efcc7f3c1ba1bac872d22c1a60efeab962a5e8
michaelsunb <s3110401@student.rmit.edu.au>
2013-08-25 01:44:16 +1000

    can paginate and search wine

--- index.php
+++ index.php
@@ -1,35 +1,153 @@
 <?php
-require_once('connect.php');
+require_once('model_region.php');
+require_once('model_grapevariety.php');
+require_once('model_winevariety.php');
 
-$model = connect::singleton();
-$region_results = $model->query_region();
-$grape_variety_results = $model->query_grape_variety();
+DEFINE("ADD_TO_LIMIT",30);
 
+$model_region = new ModelRegion();
+$region_results = $model_region->query_region();
+$model_grape_varity = new ModelGrapeVariety();
+$grape_variety_results = $model_grape_varity->query_grape_variety();
+
+$winesearch = "";
+if(isset($_GET['winesearch']))
+{
+   $winesearch = $_GET['winesearch'];
+}
+
+$region = 0;
+if(isset($_GET['region']))
+{
+   $region = $_GET['region'];
+}
+
+$grape_variety = 0;
+if(isset($_GET['grape_variety']))
+{
+   $grape_variety = $_GET['grape_variety'];
+}
+
+if(!isset($_GET['next']) ||
+   (isset($_GET['next']) && $_GET['next'] <= 0) ||
+   (isset($_GET['next']) && !preg_match("/^[0-9]+$/", $_GET['next'])))
+{
+   $limit_start = DEFAULT_START_LIMIT;
+   $limit_end = DEFAULT_END_LIMIT;
+   $prev_link = DEFAULT_START_LIMIT;
+   $next_link = DEFAULT_END_LIMIT;
+}
+else
+{
+   $limit_start = $_GET['next'];
+   $limit_end = $_GET['next'] + ADD_TO_LIMIT;
+
+   $prev_link = $_GET['next'] - ADD_TO_LIMIT;
+   $next_link = $limit_end;
+}
+
+$model_wine = new ModelWineVariety();
+$wine_results = $model_wine->search_wine_name($winesearch,$limit_start,$limit_end);
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<!DOCTYPE HTML>
 <html>
-<head></head>
+<head><title>Michael Baluyos Assignment 1</title></head>
 <body>
-<h1>Region</h1>
-<select>
+   <form action="index.php" method="get">
+      <input type="hidden" name="next" value="<?= $limit_start; ?>"> 
+      Region: 
+      <select name="region">
 <?
 foreach($region_results as $row)
 {
-   ?><option value="<? echo $row['region_id']; ?>"><? echo $row['region_name']; ?></option><?
-   echo "\n";
+   echo "\n         ";
+   if($region == $row['region_id'])
+   {
+      ?><option value="<?= $row['region_id']; ?>" selected><?= $row['region_name']; ?></option><?
+   }
+   else
+   {
+      ?><option value="<?= $row['region_id']; ?>"><?= $row['region_name']; ?></option><?
+   }
 }
 ?>
-<option value="all">All</option>
-</select>
-<h1>Grape Variety</h1>
-<select>
+
+      </select>
+      <br />
+      Grape Variety: 
+      <select name="grape_variety">
+         <option value="all">All</option>
 <?
 foreach($grape_variety_results as $row)
 {
-   ?><option value="<? echo $row['variety_id']; ?>"><? echo $row['variety']; ?></option><?
-   echo "\n";
+   echo "\n         ";
+   if($grape_variety == $row['variety_id'])
+   {
+      ?><option value="<?= $row['variety_id']; ?>" selected><?= $row['variety']; ?></option><?
+   }
+   else
+   {
+      ?><option value="<?= $row['variety_id']; ?>"><?= $row['variety']; ?></option><?
+   }
 }
-?><option value="all">All</option>
-</select>
-</body></html>
+?>
+
+      </select>
+      <br />
+      Search Wine: <input type="search" name="winesearch" value="<?= $winesearch; ?>">
+      <br />
+      <input type="submit">
+   </form>
+<?
+$add_gets = 'region='.$region.'&grape_variety='.$grape_variety.'&winesearch='.$winesearch;
+if(isset($wine_results) && count($wine_results) > 1)
+{
+   $html_prv_link = '<a href="?next='.$prev_link.'&'.$add_gets.'">';
+   $html_nxt_link = '<a href="?next='.$next_link .'&'.$add_gets.'">';
+   ?>   <table>
+      <thead>
+         <tr>
+            <th>Wine</th>
+            <th>Grape Variety</th>
+            <th>Year</th>
+            <th>Wine Type</th>
+            <th>Winery Name</th>
+         </tr>
+      </thead>
+      <tfoot>
+         <tr>
+            <td><?= $html_prv_link; ?>&lt;&lt; Previous</a></td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td><?= $html_nxt_link; ?>Next &gt;&gt;</a></td>
+         </tr>
+      </tfoot>
+      <tbody><?
+   foreach($wine_results as $row)
+   {
+      echo "\n";
+      ?>
+         <tr id="wine_<?= $row['id']; ?>_<?= $row['wine_id']; ?>">
+            <td><?= $row['wine_name']; ?></td>
+            <td><?= $row['variety']; ?></td>
+            <td><?= $row['year']; ?></td>
+            <td><?= $row['wine_type']; ?></td>
+            <td><?= $row['winery_name']; ?></td>
+         </tr><?
+   }
+   ?>
+
+      </tbody>
+   </table><?
+}
+else
+{
+   $html_nxt_link = '<a href="?next='.$next_link .'&'.$add_gets.'">';
+   ?><a href="?<?= $add_gets; ?>">reset pagination</a><?
+}
+?>
+
+</body>
+</html>
 
